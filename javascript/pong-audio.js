c10b5f817448776cba8d429a2ff0f313c039035e
YuraB81 <84031286+YuraB81@users.noreply.github.com>
2021-05-27 09:33:32 -0500

    Fixes

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -57,12 +57,12 @@ soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("sawsquarenoise-TitleScreen.mp3");
+export var adventureMusic = new soundFile("titlescreen-8bit.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("sawsquarenoise-Stage3.mp3");
+export var villageMusic = new soundFile("stage3-8bit.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
